5770e54b37bd8919eefaa8f9c1c2577562663ccf
kx1nQyy <loginovt13@gmail.com>
2025-03-26 12:34:46 +0300

    devCanvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+import CanvasComponent from "./Canvas/Canvas.jsx";
 
 function App() {
   return (
-    <>
-
-
-    </>
-  )
+      <div className="App">
+        <CanvasComponent />
+      </div>
+  );
 }
 
 export default App
